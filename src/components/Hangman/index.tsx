c650b9ec976432ff0d5692c03755fda3127a0f7d
GQSM <ms314006@hotmail.com>
2019-12-26 07:13:50 +0800

    [ADD] 增加 Shadows Into Light 字體，及頁面標題

---
+++ src/components/Hangman/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './index.scss';
+
+const Hungman: React.FC = (props) => {
+  const {} = props;
+  return (
+    <div className={styles.main}>
+      Hungman
+    </div>
+  );
+};
+
+export default Hungman;
