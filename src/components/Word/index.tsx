467c4ce4750e034e41384e6b8bc2416e2747a88b
GQSM <ms314006@hotmail.com>
2020-02-10 07:27:35 +0800

    [ADD] 文字會隨著作答正確或錯誤而顯示不同的顏色

--- src/components/Word/index.tsx
+++ src/components/Word/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import QuestionContext from '../../context/QuestionContext';
 import styles from './index.scss';
 
 interface WordProps {
@@ -8,10 +9,32 @@ interface WordProps {
 
 const Word: React.FC<WordProps> = (props: WordProps) => {
   const { word, clickEvent } = props;
+  const [clicked, setClicked] = useState(false);
+  const answer: any = useContext(QuestionContext);
+
+  const isAnswered = () => {
+    const target = answer.find(
+      ({ word: answerWord }) => answerWord === word.toLowerCase(),
+    );
+    if (!target) return false;
+    return target.answered;
+  };
+
+  const getFontColor = () => {
+    if (isAnswered()) return '#00FF00';
+    if (clicked) return '#FF0000';
+    return '#000000';
+  };
   return (
     <div
       className={styles.main}
-      onClick={clickEvent}
+      style={{ color: getFontColor() }}
+      onClick={() => {
+        if (!clicked) {
+          setClicked(true);
+          clickEvent();
+        }
+      }}
       onKeyDown={() => {}}
     >
       {word}
